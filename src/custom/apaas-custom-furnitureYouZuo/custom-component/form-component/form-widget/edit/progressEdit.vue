9ff80c2fe037d2552560e31ef7c6116f9ed0d076
JianXin Ye <jianxin.ye@definesys.com>
2022-11-26 17:56:49 +0800

    步骤条样式完成

--- src/custom/apaas-custom-furnitureYouZuo/custom-component/form-component/form-widget/edit/progressEdit.vue
+++ src/custom/apaas-custom-furnitureYouZuo/custom-component/form-component/form-widget/edit/progressEdit.vue
@@ -1,24 +1,143 @@
 <template>
-    <div>
-        <el-steps :active="3" finish-status="success">
-            <el-step title="创建" description="订单创建"></el-step>
-            <el-step title="提交" description="业务员提交"></el-step>
-            <el-step title="审核" description="领导审核"></el-step>
-            <el-step title="生产" description="订单生产"></el-step>
-            <el-step title="入库" description="订单入库"></el-step>
-            <el-step title="出货" description="订单出货"></el-step>
-            <el-step title="出货单" description="生成出货单"></el-step>
-            <el-step title="收款" description="订单收款"></el-step>
+    <div class="progress-edit">
+        <el-steps :active="activeIndex">
+            <el-step v-for="item, i in nodes" :key="i" :title="item.title" :description="item.desc">
+                <template #icon>
+                    <!-- 待办节点图标 -->
+                    <svg v-if="item.status === 'unactive'" xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink" fill="none" version="1.1" width="8" height="8"
+                        viewBox="0 0 8 8">
+                        <g style="mix-blend-mode:passthrough">
+                            <ellipse cx="4" cy="4" rx="4" ry="4" fill="#E0E0E0" fill-opacity="1" />
+                        </g>
+                    </svg>
+                    <!-- 当前步骤图标 -->
+                    <svg v-if="item.status === 'active'" xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink" fill="none" version="1.1" width="12" height="12"
+                        viewBox="0 0 12 12">
+                        <g style="mix-blend-mode:passthrough">
+                            <ellipse cx="6" cy="6" rx="6" ry="6" fill="#3370FF" fill-opacity="1" />
+                        </g>
+                    </svg>
+                    <!-- 已完成状态图标 -->
+                    <svg v-if="item.status === 'complted'" xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink" fill="none" version="1.1" width="8" height="8"
+                        viewBox="0 0 8 8">
+                        <g style="mix-blend-mode:passthrough">
+                            <ellipse cx="4" cy="4" rx="4" ry="4" fill="#3370FF" fill-opacity="1" />
+                        </g>
+                    </svg>
+                </template>
+            </el-step>
         </el-steps>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'ProgressEdit'
+    name: 'ProgressEdit',
+    data() {
+        return {
+            nodes: [
+                {
+                    title: '订单创建',
+                    desc: '订单创建',
+                    status: 'complted',
+                },
+                {
+                    title: '提交',
+                    desc: '业务员提交',
+                    status: 'complted',
+                },
+                {
+                    title: '审核',
+                    desc: '领导审核',
+                    status: 'complted',
+                },
+                {
+                    title: '生产',
+                    desc: '订单生产',
+                    status: 'active',
+                },
+                {
+                    title: '入库',
+                    desc: '订单入库',
+                    status: 'unactive',
+                },
+                {
+                    title: '出货',
+                    desc: '订单出货',
+                    status: 'unactive',
+                },
+                {
+                    title: '出货单',
+                    desc: '生成出货单',
+                    status: 'unactive',
+                },
+                {
+                    title: '收款',
+                    desc: '订单收款',
+                    status: 'unactive',
+                },
+            ]
+        }
+    },
+    computed: {
+        activeIndex() {
+            // 从1开始
+            return this.nodes.findIndex(n => n.status === 'active') + 1;
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="scss">
+.progress-edit {
+    margin: 227px 112px 85px;
 
+    .el-step__icon {
+        position: absolute;
+        top: -5px;
+    }
+
+    .el-step__icon {
+        width: auto;
+        height: 12px;
+    }
+
+    .el-step__line {
+        background: #E0E0E0;
+    }
+
+    .el-step__head.is-finish {
+        border-color: #3370FF;
+        color: #333333;
+    }
+
+    .el-step__title.is-finish,
+    .el-step__description.is-finish {
+        font-family: 'PingFangSC-Regular';
+        color: #333333;
+        font-size: 12px;
+        font-weight: normal;
+    }
+
+    .el-step__title.is-process,
+    .el-step__description.is-process {
+        font-family: 'PingFangSC-Regular';
+        color: #999999;
+        font-size: 12px;
+        font-weight: normal;
+    }
+
+    .el-step.is-horizontal .el-step__line {
+        top: 0;
+        height: 1px;
+    }
+
+    .el-step__icon {
+        border: none;
+        background: none;
+    }
+}
 </style>
